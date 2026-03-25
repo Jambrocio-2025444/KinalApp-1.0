1e9f4f76f3e3f1d575fe1c34ce36223c8348e62f
jambrocio-202544 <jambrocio-2025444@Kinal.edu.gt>
2026-03-24 22:47:07 -0600

    fix: se arreglo el finbyEstado ya que estaba mal escrito

--- src/main/java/com/joseAmbrocio/KinalApp/repository/UsuarioRepository.java
+++ src/main/java/com/joseAmbrocio/KinalApp/repository/UsuarioRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository <Usuarios,Integer> {
-    List<Usuarios> finbyEstado(int estado);
+    List<Usuarios> findByEstado(int estado);
 
 }
