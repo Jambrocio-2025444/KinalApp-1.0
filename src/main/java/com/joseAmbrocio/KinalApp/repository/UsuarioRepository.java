f8fc498d7d0e530d54b75bb2bf95fb2f55b99bc9
jambrocio-202544 <jambrocio-2025444@Kinal.edu.gt>
2026-03-24 21:32:12 -0600

    feat: agrega repository de Usuarios.java

---
+++ src/main/java/com/joseAmbrocio/KinalApp/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.joseAmbrocio.KinalApp.repository;
+
+import com.joseAmbrocio.KinalApp.entity.Usuarios;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UsuarioRepository extends JpaRepository <Usuarios,Integer> {
+    List<Usuarios> finbyEstado(int estado);
+
+}
