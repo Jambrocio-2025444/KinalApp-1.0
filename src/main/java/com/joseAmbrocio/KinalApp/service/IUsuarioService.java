8f18c13ddc37f5830bf2b63425cdcd900772acdc
jambrocio-202544 <jambrocio-2025444@Kinal.edu.gt>
2026-03-24 22:54:11 -0600

    feat: agrega Interfaz de Usuario en service

---
+++ src/main/java/com/joseAmbrocio/KinalApp/service/IUsuarioService.java
@@ -0,0 +1,29 @@
+package com.joseAmbrocio.KinalApp.service;
+
+import com.joseAmbrocio.KinalApp.entity.Usuarios;
+import java.util.List;
+import java.util.Optional;
+
+public interface IUsuarioService {
+
+    //Metodo que devuelve una lista de los Usuarios
+    List<Usuarios> listarTodos();
+
+    //Metodo que guarda a los usuarios
+    Usuarios guardar(Usuarios usuario);
+
+    //Metodo que busca un usuario por su codigoUsuario
+    Optional<Usuarios> buscarPorId(int codigoUsuario);
+
+    //Metodo que actualiza por codigoUsuario
+    Usuarios actualizar(int codigoUsuario, Usuarios usuario);
+
+    //Metodo que elimina un usuario por codigoUsuario
+    void eliminar(int codigoUsuario);
+
+    //Metodo que devuelve true si existe o false si no
+    boolean existePorId(int codigoUsuario);
+
+    //Metodo que dice si usuario esta activo o no
+    List<Usuarios> activo();
+}
