bc4e025f8fe3076214b69d930821523a2961d3b7
jambrocio-202544 <jambrocio-2025444@Kinal.edu.gt>
2026-03-28 00:58:16 -0600

    feat: se agrega el repository de Detalle Venta

---
+++ src/main/java/com/joseAmbrocio/KinalApp/repository/DetalleVentasRepository.java
@@ -0,0 +1,7 @@
+package com.joseAmbrocio.KinalApp.repository;
+
+import com.joseAmbrocio.KinalApp.entity.DetalleVentas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleVentasRepository extends JpaRepository<DetalleVentas, Long> {
+}
